70af7209a77a0ef9a2ac92cd6fb0d56b03b7b1f4
luozheao <815149054@qq.com>
2017-08-04 16:44:28 +0800

    '第一阶段结束'

---
+++ js/Observer.js
@@ -0,0 +1,57 @@
+/**
+ * Created by luozheao on 2017/8/3.
+ */
+
+function Dep() {
+    this.subs = [];
+}
+Dep.prototype = {
+    addSub: function(sub) {
+        this.subs.push(sub);
+    },
+    notify: function() {
+        this.subs.forEach(function(sub) {
+            sub.update();
+        });
+    }
+};
+var data = {name: 'kindeng'};
+observe(data);
+data.name = 'dmq'; // 哈哈哈，监听到值变化了 kindeng --> dmq
+
+
+function observe(data) {
+    if (!data || typeof data !== 'object') {
+        return;
+    }
+    // 取出所有属性遍历
+    Object.keys(data).forEach(function (key) {
+        defineReactive(data, key, data[key]);
+    });
+};
+
+function defineReactive(data, key, val) {
+    var dep = new Dep();
+    console.log(dep);
+
+    observe(val); // 监听子属性
+    Object.defineProperty(data, key, {
+        enumerable: true, // 可枚举
+        configurable: false, // 不能再define
+        get: function() {
+            Dep.target && dep.addDep(Dep.target);
+            return val;
+        },
+        set: function(newVal) {
+            if (val === newVal) return;
+            console.log('哈哈哈，监听到值变化了 ', val, ' --> ', newVal);
+            val = newVal;
+            dep.notify(); // 通知所有订阅者
+        }
+    });
+}
+
+
+
+
+
